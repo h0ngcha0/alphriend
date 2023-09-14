5d14babdce2f7e8457e3f2a8fea18e31704f53f2
h0ngcha0 <lbs.lhc@gmail.com>
2023-09-14 14:15:08 +0200

    Refactor friend-tech.test.ts

--- test/integration/friend-tech.test.ts
+++ test/integration/friend-tech.test.ts
@@ -2,7 +2,6 @@ import { getSigners, testAddress } from '@alephium/web3-test'
 import { deployToDevnet } from '@alephium/cli'
 import { BuyShares, FriendTech, FriendTechInstance, SubjectShares } from '../../artifacts/ts'
 import { web3, Project, ONE_ALPH, subContractId, addressFromContractId, encodeAddress, binToHex, SignerProvider, ExecuteScriptResult } from '@alephium/web3'
-import { totalmem } from 'os'
 
 describe('Friend tech', () => {
   const group = 0
@@ -27,118 +26,41 @@ describe('Friend tech', () => {
     if (friendTech === undefined) {
       fail(`Friend tech contract is not deployed on group ${group}`)
     }
-    await verifyFriendTechState({ expectedTotalProtocolFee: 0n, expectedBalance: ONE_ALPH })
+    const fixture = new TestFixture(friendTech)
+    await fixture.verifyFriendTechState({ expectedTotalProtocolFee: 0n, expectedBalance: ONE_ALPH })
 
-    let price = getPrice(0n, 1n)
-    let protocolFee = getProtocolFee(price)
-    let subjectFee = getSubjectFee(price)
-    let totalPrice = price + protocolFee + subjectFee
-    let totalProtocolFee = protocolFee
-    let friendTechTotalBalance = ONE_ALPH
-    let subjectSharesTotalBalance = ONE_ALPH
-
-    // First share needs to be bought by the subject him or her self
-    await expect(buyShares({ buyer: signer2, subject: signer1.address, amount: 1n, totalPayment: ONE_ALPH + totalPrice })).rejects.toThrow(Error)
-
-    // Buy 1st share
-    await buyShares({ buyer: signer1, subject: signer1.address, amount: 1n, totalPayment: ONE_ALPH })
-    await verifyFriendTechState({ expectedTotalProtocolFee: totalProtocolFee, expectedBalance: friendTechTotalBalance })
-    await verifySubjectSharesState({ subject: signer1.address, expectedSupply: 1n, expectedBalance: subjectSharesTotalBalance })
+    const price = getPrice(0n, 1n)
+    await fixture.buyShares({
+      buyer: signer1,
+      subject: signer1.address,
+      amount: 1n,
+      expectedPrice: price
+    })
 
     // Buy 2nd share
-    price = getPrice(1n, 1n)
-    protocolFee = getProtocolFee(price)
-    subjectFee = getSubjectFee(price)
-    totalPrice = price + protocolFee + subjectFee
-    totalProtocolFee = totalProtocolFee + protocolFee
-    friendTechTotalBalance = friendTechTotalBalance + protocolFee + price
-    subjectSharesTotalBalance = subjectSharesTotalBalance + subjectFee
-
-    console.log("protocolFee", protocolFee)
-    await buyShares({ buyer: signer2, subject: signer1.address, amount: 1n, totalPayment: ONE_ALPH + totalPrice })
-    await verifyFriendTechState({ expectedTotalProtocolFee: totalProtocolFee, expectedBalance: friendTechTotalBalance })
-    await verifySubjectSharesState({ subject: signer1.address, expectedSupply: 2n, expectedBalance: subjectSharesTotalBalance })
+    const price2 = getPrice(1n, 1n)
+    await fixture.buyShares({
+      buyer: signer2,
+      subject: signer1.address,
+      amount: 1n,
+      expectedPrice: price2
+    })
 
     // Buy 3rd ~ 5th share
-    price = getPrice(2n, 3n)
-    protocolFee = getProtocolFee(price)
-    subjectFee = getSubjectFee(price)
-    totalPrice = price + protocolFee + subjectFee
-    totalProtocolFee = totalProtocolFee + protocolFee
-    friendTechTotalBalance = friendTechTotalBalance + protocolFee + price
-    subjectSharesTotalBalance = subjectSharesTotalBalance + subjectFee
-
-    await buyShares({ buyer: signer2, subject: signer1.address, amount: 1n, totalPayment: ONE_ALPH + totalPrice })
-    await verifyFriendTechState({ expectedTotalProtocolFee: totalProtocolFee, expectedBalance: friendTechTotalBalance })
-    await verifyFriendTechState({ expectedTotalProtocolFee: 0n, expectedBalance: ONE_ALPH })
-    await verifySubjectSharesState({ subject: signer1.address, expectedSupply: 5n, expectedBalance: subjectSharesTotalBalance })
-  }, 20000)
-
-  async function buyShares(
-    input: {
-      buyer: SignerProvider,
-      subject: string,
-      amount: bigint,
-      totalPayment: bigint
-    }): Promise<ExecuteScriptResult> {
-    return BuyShares.execute(input.buyer, {
-      initialFields: {
-        subject: input.subject,
-        amount: input.amount,
-        totalPayment: input.totalPayment,
-        friendTech: friendTech?.contractId!
-      },
-      attoAlphAmount: input.totalPayment
+    const price3 = getPrice(2n, 3n)
+    await fixture.buyShares({
+      buyer: signer2,
+      subject: signer1.address,
+      amount: 3n,
+      expectedPrice: price3,
     })
-  }
-
-
-  async function verifyFriendTechState(input: { expectedBalance: bigint, expectedTotalProtocolFee: bigint }) {
-    const states = await friendTech!.fetchState()
-
-    expect(states.fields.owner).toEqual(testAddress)
-    expect(states.fields.protocolFeePercent).toEqual(500n)
-    expect(states.fields.subjectFeePercent).toEqual(500n)
-
-    expect(states.fields.totalProtocolFee).toEqual(input.expectedTotalProtocolFee)
-    const balance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(friendTech!.address)
-    expect(BigInt(balance.balance)).toEqual(input.expectedBalance)
-  }
-
-  async function verifySubjectSharesState(input: { subject: string, expectedSupply: bigint, expectedBalance: bigint }) {
-    // TODO: verify balance
-    const subjectSharesContractAddress = addressFromContractId(
-      subContractId(friendTech?.contractId!, binToHex(encodeAddress(input.subject)), group)
-    )
-    const signer1SharesContract = SubjectShares.at(subjectSharesContractAddress)
-    const signer1SharesContractState = await signer1SharesContract.fetchState()
-    expect(signer1SharesContractState.fields.subject).toEqual(input.subject)
-    expect(signer1SharesContractState.fields.supply).toEqual(input.expectedSupply)
-
-    const balance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(subjectSharesContractAddress)
-    expect(BigInt(balance.balance)).toEqual(input.expectedBalance)
-  }
+  }, 20000)
 
   function getPrice(supply: bigint, amount: bigint): bigint {
     const sum1: bigint = supply == 0n ? 0n : (supply - 1n) * (supply) * (2n * (supply - 1n) + 1n) / 6n;
     const sum2: bigint = supply == 0n && amount == 1n ? 0n : (supply - 1n + amount) * (supply + amount) * (2n * (supply - 1n + amount) + 1n) / 6n;
     const summation = sum2 - sum1
     return summation * ONE_ALPH / 16000n;
-  }
-
-  function getBuyPriceAfterFee(supply: bigint, amount: bigint): bigint {
-    const price = getPrice(supply, amount)
-    const protocolFee = getProtocolFee(price)
-    const subjectFee = getSubjectFee(price)
-    return price + protocolFee + subjectFee
-  }
-
-  function getProtocolFee(price: bigint): bigint {
-    return price * 500n / 10000n
-  }
-
-  function getSubjectFee(price: bigint): bigint {
-    return price * 500n / 10000n
   }
 
   function getSellPrice(supply: bigint, amount: bigint): bigint {
@@ -152,3 +74,85 @@ describe('Friend tech', () => {
     return price - protocolFee - subjectFee
   }
 })
+
+class TestFixture {
+  friendTech: FriendTechInstance
+  group = 0
+  totalProtocolFee: bigint
+  friendTechTotalBalance: bigint
+  subjectSharesTotalBalance: bigint
+  supply: bigint
+
+  constructor(friendTech: FriendTechInstance) {
+    this.friendTech = friendTech
+    this.totalProtocolFee = 0n
+    this.friendTechTotalBalance = ONE_ALPH
+    this.subjectSharesTotalBalance = ONE_ALPH
+    this.supply = 0n
+  }
+
+  public async buyShares(
+    input: {
+      buyer: SignerProvider,
+      subject: string,
+      amount: bigint,
+      expectedPrice: bigint
+    }): Promise<ExecuteScriptResult> {
+    const expectedProtocolFee = this.getProtocolFee(input.expectedPrice)
+    const expectedSubjectFee = this.getSubjectFee(input.expectedPrice)
+    const totalPayment = input.expectedPrice + expectedProtocolFee + expectedSubjectFee + ONE_ALPH
+    console.log("total payment", totalPayment)
+    const result = await BuyShares.execute(input.buyer, {
+      initialFields: {
+        subject: input.subject,
+        amount: input.amount,
+        totalPayment,
+        friendTech: this.friendTech.contractId!
+      },
+      attoAlphAmount: totalPayment
+    })
+
+    this.totalProtocolFee = this.totalProtocolFee + expectedProtocolFee
+    this.friendTechTotalBalance = this.friendTechTotalBalance + expectedProtocolFee + input.expectedPrice
+    this.subjectSharesTotalBalance = this.subjectSharesTotalBalance + expectedSubjectFee
+    this.supply = this.supply + input.amount
+
+    await this.verifyFriendTechState({ expectedTotalProtocolFee: this.totalProtocolFee, expectedBalance: this.friendTechTotalBalance })
+    await this.verifySubjectSharesState({ subject: input.subject, expectedSupply: this.supply, expectedBalance: this.subjectSharesTotalBalance })
+
+    return result
+  }
+
+  async verifyFriendTechState(input: { expectedBalance: bigint, expectedTotalProtocolFee: bigint }) {
+    const states = await this.friendTech.fetchState()
+
+    expect(states.fields.owner).toEqual(testAddress)
+    expect(states.fields.protocolFeePercent).toEqual(500n)
+    expect(states.fields.subjectFeePercent).toEqual(500n)
+
+    expect(states.fields.totalProtocolFee).toEqual(input.expectedTotalProtocolFee)
+    const balance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(this.friendTech.address)
+    expect(BigInt(balance.balance)).toEqual(input.expectedBalance)
+  }
+
+  async verifySubjectSharesState(input: { subject: string, expectedSupply: bigint, expectedBalance: bigint }) {
+    const subjectSharesContractAddress = addressFromContractId(
+      subContractId(this.friendTech.contractId!, binToHex(encodeAddress(input.subject)), this.group)
+    )
+    const signer1SharesContract = SubjectShares.at(subjectSharesContractAddress)
+    const signer1SharesContractState = await signer1SharesContract.fetchState()
+    expect(signer1SharesContractState.fields.subject).toEqual(input.subject)
+    expect(signer1SharesContractState.fields.supply).toEqual(input.expectedSupply)
+
+    const balance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(subjectSharesContractAddress)
+    expect(BigInt(balance.balance)).toEqual(input.expectedBalance)
+  }
+
+  getProtocolFee(price: bigint): bigint {
+    return price * 500n / 10000n
+  }
+
+  getSubjectFee(price: bigint): bigint {
+    return price * 500n / 10000n
+  }
+}
